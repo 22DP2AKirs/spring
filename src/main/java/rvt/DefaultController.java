0b0e8cdbdcc69beb3b4ae75b33bad4d63d522aee
22DP2AKirs <131154529+22DP2AKirs@users.noreply.github.com>
2023-12-14 13:41:06 +0000

    otra stunda

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
@@ -11,5 +12,19 @@ public class DefaultController {
    @GetMapping(value = "/")
     String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
         return "index";
+    }
+
+    @GetMapping(value = "/student")
+    public ModelAndView students() {
+        ModelAndView modelisUnSkats = new ModelAndView("students");
+        String name = "Peter";
+        return modelisUnSkats;
+    }
+
+    @GetMapping(value = "/about")
+    public ModelAndView about() {
+        ModelAndView modelisUnSkats = new ModelAndView("about");
+        String name = "Peter";
+        return modelisUnSkats;
     }
 }
