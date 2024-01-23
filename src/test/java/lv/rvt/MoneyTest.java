993bc4d89ceff8a112cc9b9d46475d26a4f5ec41
22DP2AKirs <131154529+22DP2AKirs@users.noreply.github.com>
2024-01-23 06:55:32 +0000

    Pabeidzu Money klases testus

--- src/test/java/lv/rvt/MoneyTest.java
+++ src/test/java/lv/rvt/MoneyTest.java
@@ -12,7 +12,6 @@ public class MoneyTest {
 
     @Test
     public void testPlusMethode() {
-
         Money macins1 = new Money(10, 10);
         Money macins2 = new Money(5, 90);
         Money macins3 = macins1.plus(macins2);
@@ -26,6 +25,18 @@ public class MoneyTest {
         Money macins2 = new Money(5, 90);
         assertFalse(macins1.lessThan(macins2));
         assertTrue(macins2.lessThan(macins1));
+    }
+
+    @Test
+    public void testMinusMethod() {
+        Money macins1 = new Money(5, 10);
+        Money macins2 = new Money(6, 0);
+
+        Money macins3 = macins1.minus(macins2);
+        Money macins4 = macins2.minus(macins1);
+
+        assertEquals(0, macins3.euros()); assertEquals(0, macins3.cents());
+        assertEquals(0, macins4.euros()); assertEquals(90, macins4.cents());
 
     }
 }
