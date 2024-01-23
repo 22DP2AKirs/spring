6b27e2c8f458dec069c88f64b73b58836d8e314e
22DP2AKirs <131154529+22DP2AKirs@users.noreply.github.com>
2024-01-23 07:54:08 +0000

    .

--- src/test/java/lv/rvt/MoneyTest.java
+++ src/test/java/lv/rvt/MoneyTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
 import rvt.Money;
 
 public class MoneyTest {
@@ -15,14 +14,18 @@ public class MoneyTest {
         Money macins1 = new Money(10, 10);
         Money macins2 = new Money(5, 90);
         Money macins3 = macins1.plus(macins2);
-        assertEquals(16, macins3.euros());
-        assertEquals(0, macins3.cents());
+        Money macins4 = new Money(3, 90);
+        Money result1 = macins4.plus(macins2);
+
+        assertEquals(9, result1.euros()); assertEquals(80, result1.cents());
+        assertEquals(16, macins3.euros()); assertEquals(0, macins3.cents());
     }
 
     @Test
     public void testLessThan() {
         Money macins1 = new Money(10, 10);
         Money macins2 = new Money(5, 90);
+
         assertFalse(macins1.lessThan(macins2));
         assertTrue(macins2.lessThan(macins1));
     }
